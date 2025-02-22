4327fb3ec617d938bf58b4b43dbf4fd10eeaea1b
Penny Chase <mpchase@mac.com>
2025-02-22 12:37:24 -0500

    Metex Worker implemented as GenServer

--- metex/lib/metex/worker.ex
+++ metex/lib/metex/worker.ex
@@ -1,20 +1,69 @@
 defmodule Metex.Worker do
 
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-      _ -> "Don't know how to process this mesage"
-    end
-    loop()
+  use GenServer
+
+  @name MW
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: @name])
   end
 
-  def temperature_of(location) do
-    result = url_for(location) |> HTTPoison.get |> parse_response
-    case result do
-      {:ok, temp} -> "#{location}: #{temp} \u00b0C"
-      {:error, msg}-> "Error retrieving temperature for #{location}: #{msg}"
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
+  end
+
+  def get_stats() do
+    GenServer.call(@name, :get_stats)
+  end
+
+  def reset_stats() do
+    GenServer.cast(@name, :reset_stats)
+  end
+
+  def stop() do
+    GenServer.cast(@name, :stop)
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def terminate(reason, stats) do
+    IO.puts "Server terminated because of #{reason}"
+    IO.inspect(stats)
+    :ok
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp}°C", new_stats}
+      _ -> 
+        {:reply, :error, stats}
     end
+  end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  ## Helper Functions
+
+  def temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
   end
 
   def url_for(location) do
@@ -40,6 +89,13 @@ defmodule Metex.Worker do
       {:ok, temp}
     rescue
       _ -> {:error, "Unable to process temperature"}
+    end
+  end
+
+  def update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true -> Map.update!(old_stats, location, &(&1 + 1))
+      false -> Map.put_new(old_stats, location, 1)
     end
   end
 
